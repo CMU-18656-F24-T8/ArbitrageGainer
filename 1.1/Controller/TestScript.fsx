8a52a5840b0cfaf7c3a23be26d8ad7dd48548535
Bohan Xu <bohanx@andrew.cmu.edu>
2024-11-25 14:27:25 -0800

    Finished orderPlacement
    Hand in 2 order information.
    
    TODO: Transaction Saver

---
+++ 1.1/Controller/TestScript.fsx
@@ -0,0 +1,15 @@
+﻿open System.Text.Json.Nodes
+open FSharp.Data
+
+
+let x = """
+["Tom",4,{"college":"cmu"} ]
+"""
+
+let n = JsonValue.Parse(x)
+
+n.[0]
+
+n.[2].["college"].GetType
+
+n.[0].GetType()
